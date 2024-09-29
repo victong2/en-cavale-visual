b3480e6fbb464663e58a3ddc53cc6f7708c938b4
Victor Nguyen <victong2@gmail.com>
2024-09-29 12:20:26 -0400

    Try to speed up process with Bulk insert

--- backend/en_cavale/commands.py
+++ backend/en_cavale/commands.py
@@ -3,6 +3,8 @@ import re
 import click
 import pandas as pd
 import decimal
+import time
+from functools import wraps
 
 from flask import current_app
 
@@ -16,8 +18,21 @@ basedir = Path(__file__).resolve().parent
 datadir = basedir / "../../data"
 
 
+def timing_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} took {end_time - start_time:.4f} seconds to execute.")
+        return result
+
+    return wrapper
+
+
 @click.argument("csv_file")
 @click.command("import-csv")
+@timing_decorator
 def import_csv(csv_file):
     """Import data from a CSV file into the database."""
     file = datadir / csv_file
@@ -32,22 +47,33 @@ def import_csv(csv_file):
     # Preload all category_name to category_id mappings into a dictionary
     category_mapping = {cat.name: cat.id for cat in Category.query.all()}
 
+    # Pre-process the DataFrame
+    df["category_id"] = df["Categorie"].map(category_mapping)
+    df["amount"] = df["Montant"].str.replace(r"[^0-9|.]", "", regex=True).astype(float)
+
+    # Filter out rows with invalid categories
+    valid_df = df[df["category_id"].notna()]
+    invalid_categories = df[df["category_id"].isna()]["Categorie"].unique()
+
+    if len(invalid_categories) > 0:
+        print(f"Invalid categories found: {invalid_categories}")
+
+    spendings = []
     # Iterate through the CSV rows
-    for index, row in df.iterrows():
-        category_name = row["Categorie"]
-        category_id = category_mapping.get(category_name)
+    for index, row in valid_df.iterrows():
+        spending = Spending(
+            date=row["Date"],
+            amount=row["amount"],
+            category_id=row["category_id"],
+        )
+        if not pd.isna(row["Description"]):
+            spending.description = row["Description"]
+        spendings.append(spending)
+        db.session.add(spending)
 
-        if category_id:
-            spending = Spending(
-                date=row["Date"],
-                amount=float(re.sub("[^0-9|.]", "", row["Montant"])),
-                category_id=category_id,
-            )
-            if not pd.isna(row["Description"]):
-                spending.description = row["Description"]
-            db.session.add(spending)
-        else:
-            print(f"Category '{category_name}' not found at index {index}!")
-
+    # Bulk insert not faster that inserting one by one and then committing.
+    # db.session.bulk_save_objects(spendings)
     db.session.commit()
-    print(f"Data from {csv_file} successfully imported.")
+    print(
+        f"Data from {csv_file} successfully imported. {len(spendings)} spendings inserted."
+    )
